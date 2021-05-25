9b122f9c523aa18febe4e6d63aed8a5b0c534111
hqrkatsu <82870363+hqrkatsu@users.noreply.github.com>
2021-05-25 14:20:44 +0900

    Second Commit
    vv

--- js/slide.js
+++ js/slide.js
@@ -3,15 +3,28 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 4000,
         dots: true,
-        arrow: false,
+        arrows: false,
         centerMode: true,
         slidesToShow: 1,
-        variableWidth: true
+        variableWidth: true,
+        adaptiveHeight: true,
+        centerPadding: '100px',
+        focusOnSelect: true
     });
 
     $(".historySlider").slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         variableWidth: true,
+        arrows: true,
+        adaptiveHeight: true,
+        infinite:true,
+        responsive: [{
+            breakpoint: 555,
+                 settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+            }
+       }]
     });
 });
